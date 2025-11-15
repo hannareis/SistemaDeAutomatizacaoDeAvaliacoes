b0e98dec3b370ca2d015576747f38f13e236522d
Hanna Reis <reishanna157@gmail.com>
2025-11-15 15:42:50 -0400

    finalizacao

--- SistemaDeAutomatizacaoDeAvaliacoes/MenuNotas.cpp
+++ SistemaDeAutomatizacaoDeAvaliacoes/MenuNotas.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <limits>
 #include "MenuNotas.hpp"
-#include "Projetos.hpp"
-#include "Notas.hpp"
+#include "Notas.hpp"   // apenas ele
 
 using namespace std;
 
@@ -12,17 +11,21 @@ void MenuNotas() {
         cout << "\n=== Menu de Notas ===\n";
         cout << "1. Criar Notas\n";
         cout << "2. Listar Notas\n";
-
         cout << "0. Voltar\n";
         cout << "Escolha: ";
+
         cin >> opcao;
-       cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         switch (opcao) {
         case 1: criarNotas(); break;
         case 2: listarNotas(); break;
-        case 0: cout << "Voltando ao menu principal...\n"; break;
-        default: cout << "Opção inválida!\n";
+        case 0:
+            cout << "Voltando ao menu principal...\n";
+            break;
+        default:
+            cout << "Opção inválida!\n";
         }
+
     } while (opcao != 0);
 }
